592727baf78353c340a02086ecfe19a5013fa88e
deranjer <deranjer@gmail.com>
2026-04-30 09:50:00 -0400

    fix: fix docs build

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -10,7 +10,11 @@ const config: Config = {
   baseUrl: "/",
 
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
 
   i18n: {
     defaultLocale: "en",
